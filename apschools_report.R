4e8a3d5fb8d5910621a7145357268632e3798a76
Vijay Mudivedu <Vijay.Mudivedu@iiitb.net>
2018-06-01 19:12:00 +0530

    inital commit

---
+++ apschools_report.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+kadapa_schools <- read.csv(file = "ap janma/kadapa.csv")
+str(kadapa_schools)
+head(kadapa_schools)
+anantapur_schools <- read.csv(file = "ap janma/anantapur_schools.csv")
+chittor_schools <- read.csv(file = "ap janma/chittor_schools.csv")
